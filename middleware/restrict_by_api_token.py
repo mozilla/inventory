036bfca0fdb0a9c9b7ebe1b09da2e4c18e39baf4
Rob Tucker <rtucker@mozilla.com>
2011-11-15 05:34:58 -0800

    Adding support for new auth method

---
+++ middleware/restrict_by_api_token.py
@@ -0,0 +1,37 @@
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseForbidden
+
+
+
+
+class RestrictByToken:
+    supports_object_permissions = False
+    supports_anonymous_user = False
+    def authenticate(self, username=None, password=None, token=None):
+        print "The path is %s" % request.path
+        username = None
+        password = None
+        token = None
+        try:
+            the_part = base64.b64decode(request.META['HTTP_AUTHORIZATION'].split()[1]).split(':',1)
+            username = the_part[0]
+            password = the_part[1]
+        except:
+            username = request.user
+            if 'token' in request.GET:
+                token = request.GET['token']
+
+
+
+        try:
+            user = authenticate(username=username, token=token, password=password)
+        except:
+            return HttpResponseForbidden('You are not authorized to view this resource')
+        return None
+
+    def get_user(self, user_id):
+            try:
+                return User.objects.get(pk=user_id)
+            except User.DoesNotExist:
+                return None
