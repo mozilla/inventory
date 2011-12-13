b3ad52d5e830dde665e14d1cdd2e7accbafa6da4
Rob Tucker <rtucker@mozilla.com>
2011-12-13 13:42:03 -0800

    Added more follow=True to api/tests.py for client.put and client.delete

--- api/tests.py
+++ api/tests.py
@@ -51,7 +51,7 @@ class SystemApi(TestCase):
         self.assertEqual(200, resp.status_code)
 
     def test_create_system(self):
-        resp = self.client.post('/api/system/%s/' % self.new_hostname)
+        resp = self.client.post('/api/system/%s/' % self.new_hostname, follow=True)
         self.assertEqual(201, resp.status_code)
         resp = self.client.get('/api/system/%i/' % 3, follow=True)
         self.assertEqual(200, resp.status_code)
@@ -60,18 +60,18 @@ class SystemApi(TestCase):
         self.assertEqual(json.loads(resp.content)['hostname'], self.new_hostname)
 
     def test_update_system(self):
-        resp = self.client.post('/api/system/%s/' % self.new_hostname)
+        resp = self.client.post('/api/system/%s/' % self.new_hostname, follow=True)
         self.assertEqual(201, resp.status_code)
-        resp = self.client.put('/api/system/%i/' % (self.new_host_id), {'hostname':'updated_hostname'})
+        resp = self.client.put('/api/system/%i/' % (self.new_host_id), {'hostname':'updated_hostname'}, follow=True)
         self.assertEqual(200, resp.status_code)
         resp = self.client.get('/api/system/%i/' % (self.new_host_id), follow=True)
         self.assertEqual(200, resp.status_code)
         self.assertEqual(json.loads(resp.content)['hostname'], 'updated_hostname')
 
     def test_delete_system(self):
-        resp = self.client.post('/api/system/%s/' % self.new_hostname)
+        resp = self.client.post('/api/system/%s/' % self.new_hostname, follow=True)
         self.assertEqual(201, resp.status_code)
-        resp = self.client.delete('/api/system/%i/' % (4))
+        resp = self.client.delete('/api/system/%i/' % (4), follow=True)
         self.assertEqual(200, resp.status_code)
         resp = self.client.get('/api/system/%i/' % (self.new_host_id), follow=True)
         self.assertEqual(404, resp.status_code)
@@ -142,5 +142,5 @@ class DHCPApi(TestCase):
         system_list = json.loads(resp.content)
         #print system_list
     def test_delete_network_adapter(self):
-        resp = self.client.delete('/api/keyvalue/1/', {'system_hostname':'fake-hostname2', 'adapter_number':'0', 'key_type':'delete_network_adapter'})
+        resp = self.client.delete('/api/keyvalue/1/', {'system_hostname':'fake-hostname2', 'adapter_number':'0', 'key_type':'delete_network_adapter'}, follow=True)
         #print "The content is %s" % resp.content
