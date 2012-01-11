d4525dcd8082822617289ce18e058affcd06e6cc
Rob Tucker <rtucker@mozilla.com>
2012-01-11 13:43:01 -0800

    Added unit tests for systems search. Resolved Issue #66

--- systems/tests.py
+++ systems/tests.py
@@ -18,6 +18,41 @@ except:
 from inventory.systems import models
 from test_utils import setup_test_environment,TestCase
 setup_test_environment()
+class SystemDatagridTest(TestCase):
+    fixtures = ['testdata.json']
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        resp = self.client.get("/en-US/systems/list_all_systems_ajax/?_=1326311056872&sEcho=1&iColumns=3&sColumns=&iDisplayStart=0&iDisplayLength=10&sSearch=&bRegex=false&sSearch_0=&bRegex_0=false&bSearchable_0=true&sSearch_1=&bRegex_1=false&bSearchable_1=true&sSearch_2=&bRegex_2=false&bSearchable_2=true&iSortingCols=1&iSortCol_0=0&sSortDir_0=asc&bSortable_0=true&bSortable_1=true&bSortable_2=false", follow=True)
+        self.assertEqual(resp.status_code, 200)
+        obj = json.loads(resp.content)
+        obj = obj['aaData']
+        self.assertEqual(len(obj),2)
+        self.assertEqual(obj[0][0], '2,fake-hostname1')
+
+    def test_blanket_search(self):
+        resp = self.client.get("/en-US/systems/list_all_systems_ajax/?_=1326311056872&sEcho=1&iColumns=3&sColumns=&iDisplayStart=0&iDisplayLength=10&sSearch=fake-hostname&bRegex=false&sSearch_0=&bRegex_0=false&bSearchable_0=true&sSearch_1=&bRegex_1=false&bSearchable_1=true&sSearch_2=&bRegex_2=false&bSearchable_2=true&iSortingCols=1&iSortCol_0=0&sSortDir_0=asc&bSortable_0=true&bSortable_1=true&bSortable_2=false", follow=True)
+        self.assertEqual(resp.status_code, 200)
+        obj = json.loads(resp.content)
+        obj = obj['aaData']
+        self.assertEqual(len(obj),2)
+        self.assertEqual(obj[0][0], '2,fake-hostname1')
+
+    def test_specific_search(self):
+        resp = self.client.get("/en-US/systems/list_all_systems_ajax/?_=1326317772224&sEcho=4&iColumns=8&sColumns=&iDisplayStart=0&iDisplayLength=10&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&sSearch=fake-hostname2&bRegex=false&sSearch_0=&bRegex_0=false&bSearchable_0=true&sSearch_1=&bRegex_1=false&bSearchable_1=true&sSearch_2=&bRegex_2=false&bSearchable_2=true&sSearch_3=&bRegex_3=false&bSearchable_3=true&sSearch_4=&bRegex_4=false&bSearchable_4=true&sSearch_5=&bRegex_5=false&bSearchable_5=true&sSearch_6=&bRegex_6=false&bSearchable_6=true&sSearch_7=&bRegex_7=false&bSearchable_7=true&iSortingCols=1&iSortCol_0=0&sSortDir_0=asc&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=true&bSortable_4=true&bSortable_5=true&bSortable_6=true&bSortable_7=false", follow=True)
+        self.assertEqual(resp.status_code, 200)
+        obj = json.loads(resp.content)
+        obj = obj['aaData']
+        self.assertEqual(len(obj),1)
+        self.assertEqual(obj[0][0], '1,fake-hostname2')
+
+    def test_failed_search(self):
+        resp = self.client.get("/en-US/systems/list_all_systems_ajax/?_=1326317772224&sEcho=4&iColumns=8&sColumns=&iDisplayStart=0&iDisplayLength=10&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&sSearch=asdfasdfasdf&bRegex=false&sSearch_0=&bRegex_0=false&bSearchable_0=true&sSearch_1=&bRegex_1=false&bSearchable_1=true&sSearch_2=&bRegex_2=false&bSearchable_2=true&sSearch_3=&bRegex_3=false&bSearchable_3=true&sSearch_4=&bRegex_4=false&bSearchable_4=true&sSearch_5=&bRegex_5=false&bSearchable_5=true&sSearch_6=&bRegex_6=false&bSearchable_6=true&sSearch_7=&bRegex_7=false&bSearchable_7=true&iSortingCols=1&iSortCol_0=0&sSortDir_0=asc&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=true&bSortable_4=true&bSortable_5=true&bSortable_6=true&bSortable_7=false", follow=True)
+        self.assertEqual(resp.status_code, 200)
+        obj = json.loads(resp.content)
+        obj = obj['aaData']
+        self.assertEqual(len(obj),0)
 
 class BlankTest(TestCase):
     fixtures = ['testdata.json']
@@ -88,7 +123,6 @@ class SimpleTest(TestCase):
         resp = self.client.post("/en-US/systems/quicksearch/", {'quicksearch':'fake-hostname2', 'is_test':'True'}, follow=True)
         self.assertEqual(resp.status_code, 200)
         obj = json.loads(resp.content)
-        print obj
         self.assertEqual(1,obj[0]['pk'])
         self.assertEqual('fake-hostname2',obj[0]['fields']['hostname'])
 
