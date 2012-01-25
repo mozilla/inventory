42693feb663c8ec72f6436309d6fd30e5dbb0d3b
Rob Tucker <rtucker@mozilla.com>
2012-01-25 05:10:17 -0800

    Changed total reporting number when returning data set to reflect proper amount of items

--- systems/views.py
+++ systems/views.py
@@ -107,8 +107,8 @@ def list_all_systems_ajax(request):
         search_q |= Q(notes__contains=search_term)
         search_q |= Q(asset_tag=search_term)
         search_q |= Q(oob_ip__contains=search_term)
-        search_q |= Q(keyvalue__value__contains=search_term)
         total_count = models.System.with_related.filter(search_q).count()
+        search_q |= Q(keyvalue__value__contains=search_term)
         end_display = int(iDisplayStart) + int(iDisplayLength)
         systems = models.System.with_related.filter(search_q).order_by('hostname').distinct('hostname')[iDisplayStart:end_display]
         the_data = build_json(request, systems, sEcho, total_count, iDisplayLength, sort_col, sort_dir)
