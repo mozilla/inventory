db151e8adff1b26e5650a7eb6d3b0fee440e4e15
uberj <uberj@onid.orst.edu>
2012-07-30 10:18:12 -0700

    removed static home dir path.

--- mdns/migrate/build_all.sh
+++ mdns/migrate/build_all.sh
@@ -44,19 +44,19 @@
 #zeusglb
 #zones
 
-BZ=/home/juber/inventory/mdns/migrate/build_migration_configs/bz_legacy.sh
-ZONE_CONFIGS=/home/juber/inventory/mdns/migrate/zone_configs
+BZ=$HOME/inventory/mdns/migrate/build_migration_configs/bz_legacy.sh
+ZONE_CONFIGS=$HOME/inventory/mdns/migrate/zone_configs
 rm -rf $ZONE_CONFIGS
 mkdir $ZONE_CONFIGS
 touch $ZONE_CONFIGS/__init__.py
 
-#$BZ cn /home/juber/sysadmins/dnsconfig/cn > $ZONE_CONFIGS/cn_zone_config.py
+#$BZ cn $HOME/sysadmins/dnsconfig/cn > $ZONE_CONFIGS/cn_zone_config.py
 #python $ZONE_CONFIGS/cn_zone_config.py
 
-#$BZ external /home/juber/sysadmins/dnsconfig/external > $ZONE_CONFIGS/external_zone_config.py
+#$BZ external $HOME/sysadmins/dnsconfig/external > $ZONE_CONFIGS/external_zone_config.py
 #python $ZONE_CONFIGS/external_zone_config.py
 
-$BZ phx /home/juber/sysadmins/dnsconfig/phx > $ZONE_CONFIGS/phx_zone_config.py
+$BZ phx $HOME/sysadmins/dnsconfig/phx > $ZONE_CONFIGS/phx_zone_config.py
 python $ZONE_CONFIGS/phx_zone_config.py
 
 ./build_migration_configs/mozilla.com_dcs.sh mozilla_com_dcs ~/sysadmins/dnsconfig/mozilla.com > $ZONE_CONFIGS/mozilla_com_dc_zone_config.py
@@ -65,6 +65,6 @@ python $ZONE_CONFIGS/mozilla_com_dc_zone_config.py
 ./build_migration_configs/zones_reverse.sh private_reverse ~/sysadmins/dnsconfig/mozilla.com > $ZONE_CONFIGS/private_reverse.py
 python $ZONE_CONFIGS/private_reverse.py
 
-./build_migration_configs/bz_legacy.sh external /home/juber/sysadmins/dnsconfig/external > $ZONE_CONFIGS/external.py
+./build_migration_configs/bz_legacy.sh external $HOME/sysadmins/dnsconfig/external > $ZONE_CONFIGS/external.py
 python $ZONE_CONFIGS/external.py
 
