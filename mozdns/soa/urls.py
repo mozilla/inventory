7d006ec25e14b5b84608bd808ad83b74260fd19a
uberj <uberj@onid.orst.edu>
2012-07-31 07:45:28 -0700

    KeyValue on SOA complete.

--- mozdns/soa/urls.py
+++ mozdns/soa/urls.py
@@ -5,8 +5,9 @@ from mozdns.soa.views import *
 
 urlpatterns = patterns('',
     url(r'^$', SOAListView.as_view()),
+    url(r'attr/$', delete_soa_attr),
     url(r'create/$', csrf_exempt(SOACreateView.as_view())),
-    url(r'(?P<pk>[\w-]+)/update/$', csrf_exempt(SOAUpdateView.as_view())),
+    url(r'(?P<soa_pk>[\w-]+)/update/$', csrf_exempt(update_soa)),
     url(r'(?P<pk>[\w-]+)/delete/$', csrf_exempt(SOADeleteView.as_view())),
     url(r'(?P<pk>[\w-]+)/$', csrf_exempt(SOADetailView.as_view())),
 )
--- mozdns/soa/views.py
+++ mozdns/soa/views.py
@@ -1,6 +1,11 @@
+from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import render
+from django.forms.util import ErrorList, ErrorDict
+
 from mozdns.domain.models import Domain
 from mozdns.soa.forms import SOAForm
-from mozdns.soa.models import SOA
+from mozdns.soa.models import SOA, SOAKeyValue
 from mozdns.utils import tablefy
 from mozdns.views import MozdnsCreateView
 from mozdns.views import MozdnsDeleteView
@@ -8,6 +13,9 @@ from mozdns.views import MozdnsDetailView
 from mozdns.views import MozdnsListView
 from mozdns.views import MozdnsUpdateView
 
+from core.keyvalue.utils import get_aa, get_docstrings, get_attrs
+from core.keyvalue.utils import update_attrs, dict_to_kv
+import simplejson as json
 
 class SOAView(object):
     model = SOA
@@ -45,9 +53,60 @@ class SOADetailView(SOAView, MozdnsDetailView):
 class SOACreateView(SOAView, MozdnsCreateView):
     """ """
 
+def delete_soa_attr(request, attr_pk):
+    """
+    An view destined to be called by ajax to remove an attr.
+    """
+    attr = get_object_or_404(SOAKeyValue, pk=attr_pk)
+    attr.delete()
+    return HttpResponse("Attribute Removed.")
 
-class SOAUpdateView(SOAView, MozdnsUpdateView):
-    """ """
+def update_soa(request, soa_pk):
+    soa = get_object_or_404(SOA, pk=soa_pk)
+    attrs = soa.soakeyvalue_set.all()
+    docs = get_docstrings(SOAKeyValue())
+    aa = get_aa(SOAKeyValue())
+    if request.method == 'POST':
+        form = SOAForm(request.POST, instance=soa)
+        try:
+            if not form.is_valid():
+                return render(request, 'soa/soa_edit.html', {
+                    'soa': soa,
+                    'form': form,
+                    'attrs': attrs,
+                    'docs': docs,
+                    'aa': json.dumps(aa)
+                })
+            else:
+                # Handle key value stuff.
+                kv = None
+                kv = get_attrs(request.POST)
+                update_attrs(kv, attrs, SOAKeyValue, soa, 'soa')
+                soa = form.save()
+                return redirect(soa.get_edit_url())
+        except ValidationError, e:
+            if form._errors is None:
+                form._errors = ErrorDict()
+            form._errors['__all__'] = ErrorList(e.messages)
+            if kv:
+                attrs = dict_to_kv(kv, SOAKeyValue)
+            return render(request, 'soa/soa_edit.html', {
+                'soa': soa,
+                'form': form,
+                'attrs': attrs,
+                'docs': docs,
+                'aa': json.dumps(aa)
+            })
+
+    else:
+        form = SOAForm(instance=soa)
+        return render(request, 'soa/soa_edit.html', {
+            'soa': soa,
+            'form': form,
+            'attrs': attrs,
+            'docs': docs,
+            'aa': json.dumps(aa)
+        })
 
 
 class SOAListView(SOAView, MozdnsListView):
