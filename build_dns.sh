39753b485d8ffda35ca825dbd108ff3b190f5c35
uberj <uberj@onid.orst.edu>
2012-08-22 11:32:49 -0700

    added && so it fails more cleanly

--- build_dns.sh
+++ build_dns.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-rm -rf ~/dnsbuilds/*
-python manage.py bindbuild
-sudo rm -rf /etc/invzones/
-sudo cp -r ~/dnsbuilds/ /etc/invzones/
-sudo chown -R named /etc/invzones/
+echo "Building..."
+rm -rf ~/dnsbuilds/* &&\
+python manage.py bindbuild &&\
+sudo rm -rf /etc/invzones/ && \
+sudo cp -r ~/dnsbuilds/ /etc/invzones/ &&\
+sudo chown -R named /etc/invzones/ &&\
+echo "Build complete." &&\
+echo "Restarting named..." &&\
+sudo /etc/init.d/named restart &&\
+echo "Restart Complete."
