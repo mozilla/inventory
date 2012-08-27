ac72550c5296df12c5ca025f5a0da649693ef0a1
uberj <uberj@onid.orst.edu>
2012-08-27 13:13:09 -0700

    master form tests and app.

--- mozdns/tests/all.py
+++ mozdns/tests/all.py
@@ -2,3 +2,4 @@ from mozdns.tests.model_tests import *
 from mozdns.domain.view_tests import *
 from mozdns.soa.view_tests import *
 from mozdns.view_tests import *
+from mozdns.master_form.tests import *
