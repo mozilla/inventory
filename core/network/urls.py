6a2bded942f84151493df37e4405820dc1e284c4
uberj <uberj@onid.orst.edu>
2012-10-11 09:23:50 -0700

    removed the network wizard... for now. I'll probably rewrite this.

--- core/network/urls.py
+++ core/network/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls.defaults import *
 
 from core.network.views import *
-from core.network.network_wizard import *
 
 
 urlpatterns = patterns('',
     url(r'^$', NetworkListView.as_view()),
-    url(r'^wizard_create/$', network_wizard),
     url(r'^create/$', create_network),
     url(r'^(?P<network_pk>[\w-]+)/$', network_detail),
     url(r'^(?P<network_pk>[\w-]+)/update/$', update_network),
