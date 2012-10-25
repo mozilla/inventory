adfdd2ffd3c6cd7cdffde37ad3d10b91d4602cff
uberj <uberj@onid.orst.edu>
2012-10-25 14:46:42 -0700

    small changes to the search results for SOA

--- core/search/management/commands/compile_test.py
+++ core/search/management/commands/compile_test.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from core.search.compiler import compiler
+from core.search.compiler import invparse
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
