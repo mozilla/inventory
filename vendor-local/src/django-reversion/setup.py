8c1f7925fbe353341ca796abb19288be41fd60fd
uberj <uberj@onid.orst.edu>
2013-01-25 17:46:37 -0800

    soooooo, yeah. vendor-local isn't using submodules
    This kind of sucks. When we fix this, checkout django-reversions at the commit 3213992ad1a528980af8f5654798643031a96659

---
+++ vendor-local/src/django-reversion/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+
+# Load in babel support, if available.
+try:
+    from babel.messages import frontend as babel
+    cmdclass = {"compile_catalog": babel.compile_catalog,
+                "extract_messages": babel.extract_messages,
+                "init_catalog": babel.init_catalog,
+                "update_catalog": babel.update_catalog,}
+except ImportError:
+    cmdclass = {}
+
+
+setup(name="django-reversion",
+      version="1.5.2",
+      description="An extension to the Django web framework that provides comprehensive version control facilities",
+      author="Dave Hall",
+      author_email="dave@etianen.com",
+      url="http://github.com/etianen/django-reversion",
+      download_url="http://github.com/downloads/etianen/django-reversion/django-reversion-1.5.2.tar.gz",
+      zip_safe=False,
+      packages=["reversion", "reversion.management", "reversion.management.commands", "reversion.migrations"],
+      package_dir={"": "src"},
+      package_data = {"reversion": ["locale/*/LC_MESSAGES/django.*", "templates/reversion/*.html"]},
+      cmdclass = cmdclass,
+      classifiers=["Development Status :: 5 - Production/Stable",
+                   "Environment :: Web Environment",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: BSD License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Framework :: Django",])
