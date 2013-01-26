26a1db94123b32134201792322e9f3660b12d12a
uberj <uberj@onid.orst.edu>
2013-01-25 21:51:09 -0800

    implement single zone import management command

--- migrate_dns/management/commands/dns_migrate.py
+++ migrate_dns/management/commands/dns_migrate.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from migrate_dns.private_public_import import do_import
+from migrate_dns.import_utils import do_import
 
 
 class Command(BaseCommand):
