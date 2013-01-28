da68f1e75ad327a068558f568ed3a279dc526e1e
uberj <uberj@onid.orst.edu>
2013-01-28 12:39:08 -0800

    updated view to adhere to build interface

--- mozdns/mozbind/views.py
+++ mozdns/mozbind/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from mozdns.soa.models import SOA
+from mozdns.view.models import View
 from mozdns.mozbind.zone_builder import build_zone_data
 
 import simplejson as json
@@ -13,7 +14,15 @@ def build_debug_soa(request, soa_pk):
     # DEBUG_BUILD_STRING = build_zone(soa, root_domain)
     # Figure out what sort of domains are in this zone.
     try:
-        private_data, public_data = build_zone_data(soa.root_domain, soa)
+        private_view = View.objects.get(name='private')
+        public_view = View.objects.get(name='public')
+
+        private_data = build_zone_data(private_view, soa.root_domain, soa)
+        private_data = private_data.format(serial=soa.serial)
+
+        public_data = build_zone_data(public_view, soa.root_domain, soa)
+        public_data = private_data.format(serial=soa.serial)
+
         output = _(
             """
 ;======= Private Data =======
