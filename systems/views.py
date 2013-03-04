926f6ee82e2fae71d2a5df0f9dc195f286285dcf
uberj <uberj@onid.orst.edu>
2013-03-04 10:01:13 -0800

    fixed oncall

--- systems/views.py
+++ systems/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import  redirect, get_object_or_404
+from django.shortcuts import  redirect, get_object_or_404, render
 from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.utils import translation
