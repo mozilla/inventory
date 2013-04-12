5efe33b14f59e788690880ff13969d3d4e3293a1
uberj <uberj@onid.orst.edu>
2013-04-12 10:37:02 -0700

    fixed multiple bugs in the search DSL

--- core/search/compiler/invparsley.py
+++ core/search/compiler/invparsley.py
@@ -12,14 +12,15 @@ grammar = """
 #   - compile
 
 ws = ' '*
+wss = ' '+
 not_ws = :c ?(c not in (' ', '\t')) -> c
 letter = :c ?('a' <= c <= 'z' or 'A' <= c <= 'Z') -> c
 special = '_' | '.' | '-' | ':' | ','
 
 # Lexical Operators
 NOT = '!'
-AND = <letter+>:and_ ?(and_.lower() == 'and') -> self.AND_op()
-OR = <letter+>:or_ ?(or_.lower() == 'or') -> self.OR_op()
+AND = <letter+>:and_ ?(and_ == 'AND') -> self.AND_op()
+OR = <letter+>:or_ ?(or_ == 'OR') -> self.OR_op()
 
 # Directive
 EQ = '=:'
@@ -31,7 +32,7 @@ DRCT = <d_lhs+>:d EQ <d_rhs+>:v -> self.directive(d, v)
 RE = '/' <(not_ws)+>:r -> self.regexpr(r)
 
 # Regular text
-text = (~OR ~AND ~NOT (letterOrDigit | special ))+
+text = (~OR ~AND ~NOT <(letterOrDigit | special )+>:t) -> t
 TEXT = <text+>:t -> self.text(t)
 
 
@@ -53,13 +54,13 @@ parens = '(' ws expr:e ws ')' -> e
 # 3) e_or
 
 # x AND y  <-- Explicit AND
-e_and = AND:op ws value:v -> (op, v)
+e_and = AND:op wss value:v -> (op, v)
 
 # x y  <-- Implicit AND
 i_and = (' '+ ~OR ~AND) value:v -> (self.AND_op(), v)
 
 # x OR y  <-- Explicit OR
-e_or = OR:op ws expr_2:v -> (op, v)
+e_or = OR:op wss expr_2:v -> (op, v)
 
 
 # Compile
--- core/search/tests/parser_tests.py
+++ core/search/tests/parser_tests.py
@@ -87,7 +87,7 @@ class ANDTest(T):
         self.assertTrue(callable(self.parse(t)))
 
     def test2(self):
-        t = 'aND'
+        t = 'AND'
         self.assertTrue(callable(self.parse(t)))
 
 
@@ -99,7 +99,7 @@ class ORTest(T):
         self.assertTrue(callable(self.parse(t)))
 
     def test2(self):
-        t = 'oR'
+        t = 'OR'
         self.assertTrue(callable(self.parse(t)))
 
 
@@ -212,4 +212,14 @@ class EXPRTests(T):
     def test26(self):
         t = "type=:foo.bar !type=:baz"
         out = "(('type', 'foo.bar') AND (NOT ('type', 'baz')))"
+        self.assertEqual(out, self.parse(t))
+
+    def test27(self):
+        t = "mozillaecuador.org OR mozillaecuador.org"
+        out = "(mozillaecuador.org OR mozillaecuador.org)"
+        self.assertEqual(out, self.parse(t))
+
+    def test28(self):
+        t = "mozillaecuadOR.org"
+        out = "mozillaecuadOR.org"
         self.assertEqual(out, self.parse(t))
