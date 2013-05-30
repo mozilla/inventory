fbfc1980452b25cf81567b3e84bc3d003041786c
uberj <uberj@onid.orst.edu>
2013-05-29 17:53:45 -0700

    rewrote static reg migrations

--- core/registration/static/migrations/0001_initial.py
+++ core/registration/static/migrations/0001_initial.py
@@ -3,7 +3,6 @@ import datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
-from django.contrib.contenttypes.models import ContentType
 
 
 class Migration(SchemaMigration):
@@ -11,13 +10,16 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Remove StaticInterface
         # XXX PLEASE uncomment this in production
-        #db.delete_table('static_interface')
-        #db.delete_table('static_inter_key_value')
-        #db.delete_table('static_interface_views')
-        #for content_type in ContentType.objects.filter(app_label='static_intr'):
-        #    content_type.delete()
+        from django.contrib.contenttypes.models import ContentType
+
+        db.delete_table('static_interface')
+        db.delete_table('static_inter_key_value')
+        db.delete_table('static_interface_views')
+        for content_type in ContentType.objects.filter(app_label='static_intr'):
+            content_type.delete()
+
         # Adding model 'StaticReg'
-        db.create_table('static', (
+        db.create_table('static_reg', (
             ('domain', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['domain.Domain'])),
             ('label', self.gf('django.db.models.fields.CharField')(max_length=63, null=True, blank=True)),
             ('fqdn', self.gf('django.db.models.fields.CharField')(db_index=True, max_length=255, null=True, blank=True)),
@@ -28,16 +30,16 @@ class Migration(SchemaMigration):
             ('ip_lower', self.gf('django.db.models.fields.BigIntegerField')(null=True, blank=True)),
             ('ip_type', self.gf('django.db.models.fields.CharField')(max_length=1)),
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('reverse_domain', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='staticregdomain_set', null=True, to=orm['domain.Domain'])),
+            ('reverse_domain', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='reverse_staticreg_set', null=True, to=orm['domain.Domain'])),
             ('system', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['systems.System'], null=True, blank=True)),
         ))
         db.send_create_signal('static', ['StaticReg'])
 
         # Adding unique constraint on 'StaticReg', fields ['ip_upper', 'ip_lower', 'label', 'domain']
-        db.create_unique('static', ['ip_upper', 'ip_lower', 'label', 'domain_id'])
+        db.create_unique('static_reg', ['ip_upper', 'ip_lower', 'label', 'domain_id'])
 
         # Adding M2M table for field views on 'StaticReg'
-        m2m_table_name = db.shorten_name('static_views')
+        m2m_table_name = db.shorten_name('static_reg_views')
         db.create_table(m2m_table_name, (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
             ('staticreg', models.ForeignKey(orm['static.staticreg'], null=False)),
@@ -66,13 +68,13 @@ class Migration(SchemaMigration):
         db.delete_unique('static_key_value', ['key', 'value', 'obj_id'])
 
         # Removing unique constraint on 'StaticReg', fields ['ip_upper', 'ip_lower', 'label', 'domain']
-        db.delete_unique('static', ['ip_upper', 'ip_lower', 'label', 'domain_id'])
+        db.delete_unique('static_reg', ['ip_upper', 'ip_lower', 'label', 'domain_id'])
 
         # Deleting model 'StaticReg'
-        db.delete_table('static')
+        db.delete_table('static_reg')
 
         # Removing M2M table for field views on 'StaticReg'
-        db.delete_table(db.shorten_name('static_views'))
+        db.delete_table(db.shorten_name('static_reg_views'))
 
         # Deleting model 'StaticRegKeyValue'
         db.delete_table('static_key_value')
@@ -102,11 +104,11 @@ class Migration(SchemaMigration):
             'primary': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'refresh': ('django.db.models.fields.PositiveIntegerField', [], {'default': '180'}),
             'retry': ('django.db.models.fields.PositiveIntegerField', [], {'default': '86400'}),
-            'serial': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1367110401'}),
+            'serial': ('django.db.models.fields.PositiveIntegerField', [], {'default': '2013052901'}),
             'ttl': ('django.db.models.fields.PositiveIntegerField', [], {'default': '3600', 'null': 'True', 'blank': 'True'})
         },
         'static.staticreg': {
-            'Meta': {'unique_together': "(('ip_upper', 'ip_lower', 'label', 'domain'),)", 'object_name': 'StaticReg', 'db_table': "'static'"},
+            'Meta': {'unique_together': "(('ip_upper', 'ip_lower', 'label', 'domain'),)", 'object_name': 'StaticReg', 'db_table': "'static_reg'"},
             'description': ('django.db.models.fields.CharField', [], {'max_length': '1000', 'null': 'True', 'blank': 'True'}),
             'domain': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['domain.Domain']"}),
             'fqdn': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
@@ -116,7 +118,7 @@ class Migration(SchemaMigration):
             'ip_type': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
             'ip_upper': ('django.db.models.fields.BigIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'label': ('django.db.models.fields.CharField', [], {'max_length': '63', 'null': 'True', 'blank': 'True'}),
-            'reverse_domain': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'staticregdomain_set'", 'null': 'True', 'to': "orm['domain.Domain']"}),
+            'reverse_domain': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'reverse_staticreg_set'", 'null': 'True', 'to': "orm['domain.Domain']"}),
             'system': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['systems.System']", 'null': 'True', 'blank': 'True'}),
             'ttl': ('django.db.models.fields.PositiveIntegerField', [], {'default': '3600', 'null': 'True', 'blank': 'True'}),
             'views': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['view.View']", 'symmetrical': 'False', 'blank': 'True'})
@@ -187,8 +189,8 @@ class Migration(SchemaMigration):
             'system_status': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['systems.SystemStatus']", 'null': 'True', 'blank': 'True'}),
             'system_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['systems.SystemType']", 'null': 'True', 'blank': 'True'}),
             'updated_on': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'warranty_end': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
-            'warranty_start': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'null': 'True', 'blank': 'True'})
+            'warranty_end': ('django.db.models.fields.DateField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
+            'warranty_start': ('django.db.models.fields.DateField', [], {'default': 'None', 'null': 'True', 'blank': 'True'})
         },
         'systems.systemrack': {
             'Meta': {'ordering': "['name']", 'object_name': 'SystemRack', 'db_table': "u'system_racks'"},
