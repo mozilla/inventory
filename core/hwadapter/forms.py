2d78f06469171606c43f03244276884a4b7aa3cb
uberj <uberj@onid.orst.edu>
2013-11-12 09:03:13 -0800

    BUG 937657 -- Sort the DHCP scopes

--- core/hwadapter/forms.py
+++ core/hwadapter/forms.py
@@ -9,11 +9,13 @@ from truth.models import Truth
 class HWAdapterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(HWAdapterForm, self).__init__(*args, **kwargs)
+
         if not self.instance.pk:
+            truths = Truth.objects.all().values_list('name', 'name')
             self.fields['dhcp_scope'] = forms.ChoiceField(
                 choices=(
                     [('', '------------')] +
-                    list(Truth.objects.all().values_list('name', 'name'))
+                    list(truths.order_by('-name'))
                 ),
                 widget=forms.Select(attrs={'class': 'dhcp-scopes'}),
                 required=False
