7cac7b4dccc99df7800f8be64d0ac7c3fe177733
uberj <jaxbr503@gmail.com>
2014-06-05 15:26:17 -0700

    Initial south migrations and random __init__.py file

---
+++ core/service/migrations/0001_initial.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Dependency'
+        db.create_table('service_dependency', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('dependant', self.gf('django.db.models.fields.related.ForeignKey')(related_name='providers', to=orm['service.Service'])),
+            ('provider', self.gf('django.db.models.fields.related.ForeignKey')(related_name='dependants', to=orm['service.Service'])),
+        ))
+        db.send_create_signal('service', ['Dependency'])
+
+        # Adding model 'Service'
+        db.create_table('service', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('parent_service', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='subservices', null=True, to=orm['service.Service'])),
+            ('site', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['site.Site'], null=True, blank=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=511, blank=True)),
+            ('category', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('business_owner', self.gf('django.db.models.fields.CharField')(default='', max_length=255)),
+            ('tech_owner', self.gf('django.db.models.fields.CharField')(default='', max_length=255)),
+            ('used_by', self.gf('django.db.models.fields.CharField')(default='', max_length=255)),
+            ('usage_frequency', self.gf('django.db.models.fields.CharField')(default='', max_length=255, blank=True)),
+            ('impact', self.gf('django.db.models.fields.CharField')(default='', max_length=255, blank=True)),
+            ('notes', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal('service', ['Service'])
+
+        # Adding M2M table for field allocations on 'Service'
+        m2m_table_name = db.shorten_name('service_allocations')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('service', models.ForeignKey(orm['service.service'], null=False)),
+            ('allocation', models.ForeignKey(orm['systems.allocation'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['service_id', 'allocation_id'])
+
+        # Adding M2M table for field systems on 'Service'
+        m2m_table_name = db.shorten_name('service_systems')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('service', models.ForeignKey(orm['service.service'], null=False)),
+            ('system', models.ForeignKey(orm['systems.system'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['service_id', 'system_id'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Dependency'
+        db.delete_table('service_dependency')
+
+        # Deleting model 'Service'
+        db.delete_table('service')
+
+        # Removing M2M table for field allocations on 'Service'
+        db.delete_table(db.shorten_name('service_allocations'))
+
+        # Removing M2M table for field systems on 'Service'
+        db.delete_table(db.shorten_name('service_systems'))
+
+
+    models = {
+        'service.dependency': {
+            'Meta': {'object_name': 'Dependency'},
+            'dependant': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'providers'", 'to': "orm['service.Service']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'provider': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'dependants'", 'to': "orm['service.Service']"})
+        },
+        'service.service': {
+            'Meta': {'object_name': 'Service', 'db_table': "'service'"},
+            'allocations': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['systems.Allocation']", 'symmetrical': 'False', 'blank': 'True'}),
+            'business_owner': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'category': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'depends_on': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['service.Service']", 'symmetrical': 'False', 'through': "orm['service.Dependency']", 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '511', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'impact': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'parent_service': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'subservices'", 'null': 'True', 'to': "orm['service.Service']"}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['site.Site']", 'null': 'True', 'blank': 'True'}),
+            'systems': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['systems.System']", 'symmetrical': 'False', 'blank': 'True'}),
+            'tech_owner': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'usage_frequency': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'used_by': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'})
+        },
+        'site.site': {
+            'Meta': {'unique_together': "(('full_name',),)", 'object_name': 'Site', 'db_table': "'site'"},
+            'full_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['site.Site']", 'null': 'True', 'blank': 'True'})
+        },
+        'systems.allocation': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Allocation', 'db_table': "u'allocations'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'systems.location': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Location', 'db_table': "u'locations'"},
+            'address': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255', 'blank': 'True'}),
+            'note': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'systems.operatingsystem': {
+            'Meta': {'ordering': "['name', 'version']", 'object_name': 'OperatingSystem', 'db_table': "u'operating_systems'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        'systems.servermodel': {
+            'Meta': {'ordering': "['vendor', 'model']", 'object_name': 'ServerModel', 'db_table': "u'server_models'"},
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'part_number': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'vendor': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        'systems.system': {
+            'Meta': {'object_name': 'System', 'db_table': "u'systems'"},
+            'allocation': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['systems.Allocation']", 'null': 'True', 'blank': 'True'}),
+            'asset_tag': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'change_password': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'hostname': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_dhcp_server': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'is_dns_server': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'is_nagios_server': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'is_puppet_server': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'is_switch': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'licenses': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'notes': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'oob_ip': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
+            'oob_switch_port': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'operating_system': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['systems.OperatingSystem']", 'null': 'True', 'blank': 'True'}),
+            'patch_panel_port': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'purchase_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'purchase_price': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'rack_order': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '6', 'decimal_places': '2', 'blank': 'True'}),
+            'ram': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'serial': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'server_model': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['systems.ServerModel']", 'null': 'True', 'blank': 'True'}),
+            'switch_ports': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'system_rack': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['systems.SystemRack']", 'null': 'True', 'blank': 'True'}),
+            'system_status': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['systems.SystemStatus']", 'null': 'True', 'blank': 'True'}),
+            'system_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['systems.SystemType']", 'null': 'True', 'blank': 'True'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'warranty_end': ('django.db.models.fields.DateField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
+            'warranty_start': ('django.db.models.fields.DateField', [], {'default': 'None', 'null': 'True', 'blank': 'True'})
+        },
+        'systems.systemrack': {
+            'Meta': {'ordering': "['name']", 'object_name': 'SystemRack', 'db_table': "u'system_racks'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['systems.Location']", 'null': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['site.Site']", 'null': 'True'})
+        },
+        'systems.systemstatus': {
+            'Meta': {'ordering': "['status']", 'object_name': 'SystemStatus', 'db_table': "u'system_statuses'"},
+            'color': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'color_code': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        'systems.systemtype': {
+            'Meta': {'object_name': 'SystemType', 'db_table': "u'system_types'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'type_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['service']
