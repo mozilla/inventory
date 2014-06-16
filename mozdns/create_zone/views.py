7269c702a88ba6c9b41f91cf59853cdf03d9f967
uberj <jaxbr503@gmail.com>
2014-06-16 10:47:54 -0700

    Refactor create_zone function to use tranransactions and to allow creation of zones on top of an existing zone

--- mozdns/create_zone/views.py
+++ mozdns/create_zone/views.py
@@ -1,59 +1,110 @@
-from gettext import gettext as gt
 import simplejson as json
 import re
 
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import transaction
 
-from mozdns.domain.models import Domain
 from mozdns.nameserver.models import Nameserver
 from mozdns.soa.models import SOA
 from mozdns.view.models import View
 from mozdns.utils import get_zones, ensure_domain, prune_tree
 
 
-def create_zone_ajax(request):
-    """This view tries to create a new zone and returns an JSON with either
-    'success' = True or 'success' = False and some errors.
+def _create_zone(root_domain, primary, contact, nss):
+    if not root_domain:
+        raise ValueError("Please specify a root_domain")
 
-    Throughout this function note that objects that are created are recorded,
-    and if an error is caught, the previously created objects are deleted. This
-    backing-out *may* be better handling by a transaction. Django has this sort
-    of thing (middleware and decorators), but I'm in a time crunch so this
-    manual deletetion will have to do.
+    if not primary:
+        raise ValueError(
+            "Please specify a primary nameserver for the SOA record."
+        )
+
+    if not contact:
+        raise ValueError(
+            "Please specify a contact address for the SOA record."
+        )
+    contact.replace('@', '.')
+
+    if not nss:
+        # They must create at least one nameserver
+        raise ValueError(
+            "You must choose an authoritative nameserver to serve this "
+            "zone."
+        )
+
+    try:
+        soa = SOA.objects.create(
+            primary=primary, contact=contact,
+            description="SOA for {0}".format(root_domain)
+        )
+    except ValidationError, e:
+        # Make how the error is being stored a little more friendly.
+        raise ValidationError(e.messages[0])
+
+    # We want all domains created up to this point to inherit their
+    # master_domain's soa so we will say inherit_soa=False and then override
+    # the return domain's SOA.
+    # Everything under this domain can be purgeable becase we will set this
+    # domain to non-purgeable. This will also allow us to call prune tree.
+    domain = ensure_domain(
+        root_domain, purgeable=False, inherit_soa=False, force=True
+    )
+    domain.soa = soa
+    # ensure_domain doesn't ensure it created the domain with purgeable equal
+    # to True.
+    domain.purgeable = False
+    domain.save()
+    _save_nss(nss, domain)
+    return domain
+
+
+def _save_nss(nss, domain):
+    # Helper function for saving the nameserver bits in the create_zone form
+    private_view, _ = View.objects.get_or_create(name='private')
+    public_view, _ = View.objects.get_or_create(name='public')
+
+    for i, (server, ttl, views) in enumerate(nss):
+        try:
+            ns, _ = Nameserver.objects.get_or_create(
+                domain=domain, ttl=ttl, server=server
+            )
+            if 'public' in views:
+                ns.views.add(public_view)
+            if 'private' in views:
+                ns.views.add(private_view)
+            ns.save()
+        except ValidationError, e:
+            suffixes = ["th", "st", "nd", "rd", ] + ["th"] * 16
+            suffixed_i = str(i + 1) + suffixes[i + 1 % 100]
+            error_field, error_val = e.message_dict.items()[0]
+            raise ValidationError(
+                "When trying to create the {0} nameserver entry, the "
+                "nameserver field '{1}' returned the error "
+                "'{2}'".format(suffixed_i, error_field, error_val[0])
+            )
+
+
+def create_zone_ajax(request):
+    """
+    This view tries to create a new zone and returns an JSON with either
+    'success' = True or 'success' = False and some errors.
     """
 
     qd = request.POST.copy()
     # See if the domain exists.
     # Fail if it already exists or if it's under a delegated domain.
     root_domain = qd.get('root_domain', None)
-
-    if not root_domain:
-        error = "Please specify a root_domain"
-        return HttpResponse(json.dumps({'success': False, 'error': error}))
-
-    if Domain.objects.filter(name=root_domain).exists():
-        error = gt("<b>{0}</b> is already a domain. To make it a new zone, "
-                   "assign it a newly created SOA.".format(root_domain))
-        return HttpResponse(json.dumps({'success': False, 'error': error}))
-
     primary = qd.get('soa_primary', None)
-    if not primary:
-        error = "Please specify a primary nameserver for the SOA record."
-        return HttpResponse(json.dumps({'success': False, 'error': error}))
     contact = qd.get('soa_contact', None)
-    if not contact:
-        error = "Please specify a contact address for the SOA record."
-        return HttpResponse(json.dumps({'success': False, 'error': error}))
-    contact.replace('@', '.')
 
     # Find all the NS entries
     nss = []
     number_re = re.compile('nameserver_(\d+)')
-    private_view, _ = View.objects.get_or_create(name='private')
-    public_view, _ = View.objects.get_or_create(name='public')
-
+    # parse nameserver bits from POST request.
+    # compile some tuples that look like:
+    #   (<server_fqdn>, <ttl>, [<view_name>,..])
     for k, server in request.POST.iteritems():
         if k.startswith('nameserver_'):
             n = number_re.search(k)
@@ -62,85 +113,32 @@ def create_zone_ajax(request):
             ns_number = n.groups()[0]
             views = []
             if qd.get('private_view_{0}'.format(ns_number), 'off') == 'on':
-                views.append(private_view)
+                views.append('private')
             if qd.get('public_view_{0}'.format(ns_number), 'off') == 'on':
-                views.append(public_view)
+                views.append('public')
             ttl = qd.get('ttl_{0}'.format(ns_number))
             if ttl and ttl.isdigit():
                 ttl = int(ttl)
             else:
                 ttl = None
             nss.append(
-                (Nameserver(server=server, ttl=ttl), views)
+                (server, ttl, views)
             )
 
-    if not nss:
-        # They must create at least one nameserver
-        error = gt("You must choose an authoritative nameserver to serve this "
-                   "zone")
-        return HttpResponse(json.dumps({'success': False, 'error': error}))
-
-    # We want all domains created up to this point to inherit their
-    # master_domain's soa. We will override the return domain's SOA.
-    # Everything under this domain can be purgeable becase we will set this
-    # domain to non-purgeable. This will also allow us to call prune tree.
-    domain = ensure_domain(
-        root_domain, purgeable=True, inherit_soa=False, force=True)
-
-    soa = SOA(
-        primary=primary, contact=contact,
-        description="SOA for {0}".format(root_domain)
-    )
     try:
-        soa.save()
-    except ValidationError, e:
-        _clean_domain_tree(domain)
-        return HttpResponse(json.dumps({'success': False,
-                                        'error': e.messages[0]}))
-    domain.purgeable = False
-    domain.save()
+        with transaction.commit_on_success():
+            domain = _create_zone(root_domain, primary, contact, nss)
+    except (ValueError, ValidationError), e:
+        return HttpResponse(json.dumps({
+            'success': False, 'error': str(e)
+        }), status=400)
 
-    private_view, _ = View.objects.get_or_create(name='private')
-    public_view, _ = View.objects.get_or_create(name='public')
-    saved_nss = []  # If these are errors, back out
-    for i, (ns, views) in enumerate(nss):
-        ns.domain = domain
-        try:
-            ns.save()
-            for view in views:
-                ns.views.add(view)
-            saved_nss.append(ns)
-        except ValidationError, e:
-            suffixes = ["th", "st", "nd", "rd", ] + ["th"] * 16
-            suffixed_i = str(i + 1) + suffixes[i + 1 % 100]
-            error_field, error_val = e.message_dict.items()[0]
-            error = gt("When trying to create the {0} nameserver entry, the "
-                       "nameserver field '{1}' returned the error "
-                       "'{2}'".format(suffixed_i, error_field, error_val[0]))
-
-            for s_ns in saved_nss:
-                s_ns.delete()
-            _clean_domain_tree(domain)
-            soa.delete()
-            return HttpResponse(json.dumps({'success': False,
-                                            'error': error}))
-    try:
-        domain.soa = soa
-        domain.save()
-    except ValidationError, e:
-        for s_ns in saved_nss:
-            s_ns.delete()
-        _clean_domain_tree(domain)
-        soa.delete()
-        return HttpResponse(json.dumps({'success': False, 'error': error}))
-
-    return HttpResponse(json.dumps(
-        {
-            'success': True,
-            'success_url': '/en-US/core/search/#q=zone=:{0}'.format(
-                domain.name
-            )
-        }))
+    return HttpResponse(json.dumps({
+        'success': True,
+        'success_url': '/en-US/core/search/#q=zone=:{0}'.format(
+            domain.name
+        )
+    }))
 
 
 def _clean_domain_tree(domain):
@@ -171,8 +169,10 @@ def create_zone(request):
                     sorted([z.name for z in get_zones()], reverse=True))
             }
         except ObjectDoesNotExist:
-            message = gt('When trying to use {0} as a template, no zone '
-                         'named {0} was found.'.format(template_zone))
+            message = (
+                "When trying to use {0} as a template, no zone "
+                "named {0} was found.".format(template_zone)
+            )
     if not context:
         context = {
             'message': message,
